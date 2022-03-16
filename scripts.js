971011376c46f45c5e7941e8431c6462aeca7afb
C-112 Fajer Almarri <70756233+C-112FajerALmarri@users.noreply.github.com>
2022-03-16 20:55:24 +0300

    done

--- scripts.js
+++ scripts.js
@@ -2,7 +2,22 @@ alert("fajer")
 console.log(17);
 console.log('im in grad 11');
 let budget = 20;
-console.log=(budget > 30 &&  budget< 80);
+console.log(budget > 30 &&  budget< 80);
 
 budget += 30;
-console.log(budget> 30 && budget< 80)
+console.log(budget> 30 && budget< 80);
+
+let grad = prompt("enter your grad")
+if (grad>= 90){
+    console.log("لقد حصلت على امتياز 🥳")  
+} else if(grad>= 80){
+    console.log("لقد حصلت على جيد جداً🤩" )  
+} else if(grad>= 70){
+    console.log( "لقد حصلت على جيد🙂"  )  
+} else if(grad>= 60){
+    console.log("لقد حصلت على مقبول😕" )
+} else if(grad>= 50){
+    console.log( "لقد حصلت على ضعيف☹️"  )  
+}else{ console.log( "راسب💔" )
+
+}
